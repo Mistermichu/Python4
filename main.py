800ac0f34d82a0941b3b047e257f48e05e8c1dc7
MichałM <michalhm@gmail.com>
2023-08-22 00:16:17 +0200

    Create User WIP

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # REPETITIVE FUNCTIONS
+def break_lines(quantity):
+    print("*" * quantity)
+
+
 def bad_attribute_value(attribute):
     print(
         f"Podana wartość: \"{attribute}\" jest niepoprawna. Spóbuj ponownie.")
@@ -11,7 +15,7 @@ def attribute_type_check(attribute_type, message):
         user_input = None
         while not isinstance(user_input, str):
             try:
-                user_input = str(input(": "))
+                user_input = str(input(":>>> "))
                 return user_input
             except ValueError:
                 user_input = bad_attribute_value(user_input)
@@ -24,47 +28,66 @@ user_id = 100
 # CREATE USER
 
 
-class Student:
-    def __init__(self, name, surname, school_class):
-        global user_id
-        self.name = name
-        self.surname = surname
-        self.school_class = school_class
-        user_id += 1
-        self.id = "S" + str(user_id)
-
-
 def create_student():
-    global user_list
+    global user_list, user_id
+    user_id += 1
     student_name = attribute_type_check(str, "Podaj imie ucznia.")
     student_surname = attribute_type_check(str, "Podaj nazwisko ucznia.")
     student_class = attribute_type_check(str, "Podaj klase ucznia.")
-    new_student = Student(student_name, student_surname, student_class)
-    user_list[new_student.id] = {
+    user_list["S" + str(user_id)] = {
         "Student": {
-            "name": new_student.name,
-            "surname": new_student.surname,
-            "class": new_student.school_class
+            "name": student_name,
+            "surname": student_surname,
+            "class": student_class.upper()
         }
     }
+    print(user_list)
+    break_lines(10)
+
+
+def create_teacher():
+    print("Hello Teacher")
+
+
+def create_class_teacher():
+    print("Hello Class Teacher")
 
 
 def create_user():
-    print("Podaj klase użytkownika, jakiego chcesz dodać do bazy:")
-    for user_type in user_class:
-        print(user_type.upper())
-    create_student()
+    select_class = None
+    while not isinstance(select_class, str):
+        break_lines(20)
+        print("Podaj klase użytkownika, jakiego chcesz dodać do bazy.")
+        for user_type in user_class:
+            print(user_type.upper())
+        select_class = attribute_type_check(
+            str, "Wprowadź \"STOP\", aby przerwać.").upper()
+        if select_class == "UCZEŃ" or select_class == "UCZEN":
+            create_student()
+        elif select_class == "NAUCZYCIEL":
+            create_teacher()
+        elif select_class == "WYCHOWAWCA":
+            create_class_teacher()
+        elif select_class == "STOP":
+            break
+        else:
+            select_class = bad_attribute_value(select_class)
+
+
 # MAIN MENU
 
 
 def main_menu():
     user_command = None
     while not isinstance(user_command, str):
+        break_lines(40)
+        print("Wprowadź komendę:")
+        print("UTWÓRZ: Przechodzi do procesu tworzenia użytkowników")
+        print("ZARZĄDZAJ: Przechodzi do procesu zarządzania użytkownikami.")
+        print("KONIEC: Kończy działanie aplikacji.")
         user_command = str(input(": ")).upper()
         if user_command == "UTWÓRZ" or user_command == "UTWORZ":
-            print("Utwórz")
             create_user()
-            print(user_list)
             user_command = None
         elif user_command == "ZARZĄDZAJ" or user_command == "ZARZADZAJ":
             print("Zarządzaj")
